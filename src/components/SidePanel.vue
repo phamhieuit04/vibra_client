4ec69cb7b7dd1798d1673f8ec4992a93f06f6cc7
duyd486 <90164590+duyd486@users.noreply.github.com>
2025-05-28 12:00:53 +0700

    Update SidePanel

--- src/components/SidePanel.vue
+++ src/components/SidePanel.vue
@@ -17,13 +17,88 @@ const useSong = useSongStore();
 const useModal = useModalStore();
 const useActivity = useActivityStore();
 const { allCategories } = storeToRefs(useActivity)
+const { currentTrack } = storeToRefs(useSong)
+const { followArtistList } = storeToRefs(useActivity)
 
+const isFollowed = ref(false) 
+
+
+async function followThisArtist() {
+    try {
+        const res = await axios.get(`http://spotify_clone_api.test/api/artist/follow/${currentTrack.value.author.id}`, {
+            'headers': {
+                'Authorization': 'Bearer ' + authStore.user.token,
+            }
+        });
+        useActivity.onUserAction();
+        isFollowed.value = !isFollowed.value
+    } catch (e) {
+        console.log(e);
+        alert('Call API thất bại');
+    }
+}
+async function unfollowThisArtist() {
+    try {
+        const res = await axios.get(`http://spotify_clone_api.test/api/library/destroy-favorite-artist/${currentTrack.value.author.id}`, {
+            'headers': {
+                'Authorization': 'Bearer ' + authStore.user.token,
+            }
+        });
+        useActivity.onUserAction();
+        isFollowed.value = !isFollowed.value
+    } catch (e) {
+        console.log(e);
+        alert('Call API thất bại');
+    }
+}
+
+watch(() => currentTrack.value, () => {
+    isFollowed.value = false
+    followArtistList.value.forEach(artist => {
+        if (artist.artist_id === currentTrack.value.author.id) {
+            isFollowed.value = true
+        }
+    })
+  }
+)
 
 onMounted(() => {
-
+    isFollowed.value = false
+    followArtistList.value.forEach(artist => {
+        if (artist.artist_id === currentTrack.value.author.id) {
+            isFollowed.value = true
+        }
+    })
 })
 </script>
-
 <template>
-    <h1 class="text-white">Side Panel nè</h1>
+    <div class="relative w-full  h-[calc(100vh-155px)] text-white bg-slate-400">
+        <div class="h-full">
+            <img class="object-cover w-full h-full opacity-50" :src="currentTrack.thumbnail_path" >
+        </div>
+
+        <div class="absolute w-11/12 p-4 transform -translate-x-1/2  bottom-5 left-1/2 bg-[#1D1512] rounded-2xl">
+            <h2 class="mb-3 text-sm font-semibold ">Giới thiệu về nghệ sĩ </h2>
+            <div class="flex items-center justify-between mb-4">
+                <div class="w-16 h-16 bg-gray-600 rounded-full">
+                    <img class="object-cover w-full h-full rounded-full" :src="currentTrack.author.avatar_path" >
+                </div>
+                <button v-if="!isFollowed" @click="followThisArtist" class="border border-[#BC4D15] text-[#BC4D15] px-4 py-2 rounded-full text-sm font-semibold hover:bg-[#BC4D15] hover:text-black transition">
+                    Theo dõi
+                </button>
+                <button v-else @click="unfollowThisArtist" class="border border-[#BC4D15] text-[#BC4D15] px-4 py-2 rounded-full text-sm font-semibold hover:bg-[#BC4D15] hover:text-black transition">
+                    Hủy theo dõi
+                </button>
+            </div>
+
+            <div>
+                <h3 class="text-2xl font-semibold">{{ currentTrack.name }}</h3>
+                <p class="text-sm text-zinc-400">{{ currentTrack.author.followers }} người theo dõi</p>
+                <p class="mt-2 text-sm">{{ currentTrack.author.description }} ...</p>
+                <h2 class="mt-3 mb-2 text-sm font-semibold ">Về bài hát bạn đang nghe </h2>
+                <p class="mt-2 text-sm">{{ currentTrack.description }} ...</p>
+            </div>
+        </div>
+    </div>
+    
 </template>
